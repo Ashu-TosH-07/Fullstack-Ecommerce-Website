0a6cff3d337417046703276d59eeebff222ee77b
Ashutosh Mishra <ashutosh12555@gmail.com>
2025-05-04 12:58:08 +0530

    Final update V1.0

--- Client_/src/paged/admin-view/features.jsx
+++ Client_/src/paged/admin-view/features.jsx
@@ -1,12 +1,12 @@
-// // function AdminFeatures() {
-// //     return (
-// //         <div>
-// //             <h1>Admin Features</h1>
-// //         </div>
-// //     )
-// // }
+function AdminFeatures() {
+    return (
+        <div>
+            <h1>Admin Features</h1>
+        </div>
+    )
+}
 
-// // export default AdminFeatures;
+export default AdminFeatures;
 
 // import React from "react";
 // import {
